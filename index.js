88a5644040615cbaa0aa86063911e525d641aae9
bohdan12 <bohdan@hotmail.es>
2025-02-09 00:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 const cluster = require('cluster');
 const os = require('os');
-const app = require('./app'); // Import your server logic
+const app = require('./app'); // Import app.js
 
 const PORT = 3000;
 
 if (cluster.isMaster) {
-  // Number of CPU cores
   const numCPUs = os.cpus().length;
+  console.log(`Master process running. Forking ${numCPUs} workers...`);
 
-  console.log(`Master process running with PID: ${process.pid}`);
-  console.log(`Forking ${numCPUs} workers...`);
-
-  // Fork a worker for each CPU core
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  // Handle worker exits and restart them
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker with PID: ${worker.process.pid} exited.`);
-    console.log('Starting a new worker...');
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} exited. Restarting...`);
     cluster.fork();
   });
 } else {
-   const server = app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Worker running on port ${PORT} with PID: ${process.pid}`);
-  });
-
-  server.on('error', (err) => {
-    console.error('Server error:', err);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Worker ${process.pid} listening on port ${PORT}`);
   });
 }
